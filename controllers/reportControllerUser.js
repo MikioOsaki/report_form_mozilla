5027c5689dd1285c63f63221db840ae343532687
mikioosaki <mikio.osaki@posteo.de>
2018-07-27 00:17:31 +0200

    Same commit again

--- controllers/reportControllerUser.js
+++ controllers/reportControllerUser.js
@@ -58,8 +58,8 @@ exports.report_create_post = [
     body('subject', 'Subject must not be empty.').isLength({ min: 1 }).trim(),
     body('bathingspot', 'Bathingspot must not be empty.').isLength({ min: 1 }).trim(),
     body('description', 'Description must not be empty.').isLength({ min: 1 }).trim(),
-    body('firstname').isAlpha(['de-DE']).optional({checkFalsy:true}).withMessage('Firstname must be alphabetic ').trim(),
-    body('lastname').isAlpha(['de-DE','ar-DZ']).optional({checkFalsy:true}).withMessage('Lastname must be alphabetic ').trim(),
+    body('firstname').trim(),
+    body('lastname').trim(),
     // geht aber lässt keine Umlaute zu und auch keine - ..göring-eckardt etc.
     // isAlpha(['de-DE','ar-DZ']).optional({checkFalsy:true}).withMessage('Lastname must be alphabetic ').trim(),
     // body('e-mail').isEmail().trim(),
