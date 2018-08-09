13093a977c215b51072311b86816bdc89e759653
mikioosaki <mikio.osaki@posteo.de>
2018-08-10 01:55:18 +0200

    Refactor comments

--- models/report.js
+++ models/report.js
@@ -4,7 +4,6 @@ var Schema = mongoose.Schema;
 
 var ReportSchema = new Schema(
   {
-    //_someId: Schema.Types.ObjectId,
     bathingspot: { type: Schema.ObjectId, ref: 'Bathingspot', required: true },
     category: [{ type: Schema.ObjectId, ref: 'Category', required: true }],
     subject: { type: String, required: true },
@@ -20,7 +19,6 @@ var ReportSchema = new Schema(
 
 // Virtual for this report instance URL.
 // Virtuals are document properties that you can get and set but that do not get persisted to MongoDB!
-// Example Mongoose doc virtual for fullname Axl Rose
 ReportSchema
   .virtual('url')
   .get(function () {
